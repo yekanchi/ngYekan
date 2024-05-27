4a5823b762b88b269746210a88a804110a94ed5c
yekanchi <morteza.academia@gmail.com>
2024-05-27 19:05:13 +0330

    publish github pages
    Took 2 hours 48 minutes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,22 @@
-import { Component, ViewChild } from "@angular/core";
-import { TestCodeEditorComponent } from "./components/test-code-editor.component";
+import {Component, ViewChild} from "@angular/core";
+import {TestCodeEditorComponent} from "./components/test-code-editor.component";
 
 @Component({
-  selector: "bpa-root",
-  template: `
-    <h1>Welcome to Angular 17.3.1</h1>
-    <test-code-editor></test-code-editor>
-    <test-date-picker></test-date-picker>
-  `,
-  styles: [],
+	selector: "bpa-root",
+	template: `
+      <h1>Welcome to Angular 17.3.1</h1>
+      <test-code-editor></test-code-editor>
+      <test-date-picker></test-date-picker>
+
+      @if (true) {
+          <div></div>
+      }
+	`,
+	styles: [],
 })
 export class AppComponent {
-  @ViewChild(TestCodeEditorComponent) testCodeEditor?: TestCodeEditorComponent;
-  constructor() {}
+	@ViewChild(TestCodeEditorComponent) testCodeEditor?: TestCodeEditorComponent;
+
+	constructor() {
+	}
 }
